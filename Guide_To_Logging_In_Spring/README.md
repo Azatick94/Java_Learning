4687afb163e3415594f1389a2221948d2b00139d
Azatick94 <azburhanov@mail.ru>
2021-03-16 21:08:49 +0300

    logging with Lombok

--- Guide_To_Logging_In_Spring/README.md
+++ Guide_To_Logging_In_Spring/README.md
@@ -1,3 +1,8 @@
 
 * <a href="https://www.baeldung.com/spring-boot-logging">Logging in Spring Boot</a>
 * <a href="https://www.baeldung.com/logback">A Guide To Logback</a>
+
+Key moments:<br>
+* log can be configured in application.properties file; 
+* the better way is to use logback-spring.xml file to write logging configuration apart from application.properties. 
+* @Slf4j, @CommonsLog, @Log4j2 annotations (Example - LombokLoggingController.java) help to minimize boilerplate code without requirement to initialize Logger class (LoggingController.java). 
---
+++ Guide_To_Logging_In_Spring/src/main/java/com/logging/LombokLoggingController.java
@@ -0,0 +1,21 @@
+package com.logging;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class LombokLoggingController {
+
+    @RequestMapping("/lombok")
+    public String index() {
+        log.trace("A TRACE Message");
+        log.debug("A DEBUG Message");
+        log.info("An INFO Message");
+        log.warn("A WARN Message");
+        log.error("An ERROR Message");
+
+        return "Howdy! Check out the Logs to see the output...";
+    }
+}
